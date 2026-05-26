1a5a157d5fa592598ab10882eec7467a6bde725b
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-05-26 04:43:31 +0200

    Program name

--- resources/plugins/build.sh
+++ resources/plugins/build.sh
@@ -3,5 +3,5 @@
 OPT="ocamlopt.opt"
 BIN="../../Sources/build"
 
-find *.ml -execdir bash -c 'echo "(Ocelot) Plugin $(basename "{}")" && \
+find *.ml -execdir bash -c 'echo "(Automates) Plugin $(basename "{}")" && \
   '"$OPT"' -g -shared -I '"$BIN"' "{}" -o "$(basename "{}" .ml).cmxs"' \;
