2d0a9b46750989796e5c37336e44428da78a9750
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-05-27 13:41:51 +0200

    Updated header

--- src/tools.ml
+++ src/tools.ml
@@ -1,21 +1,22 @@
 (*  tools.ml
- *  Copyright (C) 2014, 2015, 2016, 2017 Edouard Evangelisti
+ *  Copyright (C) 2014-2027 Edouard Evangelisti
  * 
- *  This file is part of Ocelot (OCaml Cellular Automata).
+ *  This file is part of Automates.
  *    
- *  OCelot is free software: you can redistribute it and/or modify
+ *  Automates is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  * 
- *  Ocelot is distributed in the hope that it will be useful,
+ *  Automates is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Ocelot.  If not, see <http://www.gnu.org/licenses/>
+ *  along with Automates.  If not, see <http://www.gnu.org/licenses/>
  *)
+
  
 open Scanf
 open Printf
